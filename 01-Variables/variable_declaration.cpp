c642cf3d1c9eaa169a89ca1a9a1104db146910b2
Shiva Pradhan <shivapradhan.cybersec@gmail.com>
2026-07-11 17:33:19 +0545

    Added variable declaration example

--- 01-Variables/variable_declaration.cpp
+++ 01-Variables/variable_declaration.cpp
@@ -1,8 +1,10 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int name;
-    int age;
+    int name; //variable declaration
+    int age = 25; //variable initialization
     char ch;
+    ch = 'a'; //variable assignment
+    ch = 'b'; //variable updation
     
 }
---
+++ 03-Loop/01For_loop.cpp
@@ -0,0 +1,11 @@
+//For Loops 
+//syntax for(initialization; condition; increment/decrement) {code}
+#include<iostream>
+using namespace std;
+int main(){
+    int i;
+    for(i = 0; i<10; i++){
+        cout<<i<<" "<<"Thank you"<<endl;
+    }
+    return 0;
+}
